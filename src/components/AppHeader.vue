6a25e492b4e27170d795a5f7203a6890fa402055
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-02-14 17:05:35 +0100

    style header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,40 +5,75 @@
 </script>
 
 <template>
-    <div>
-        <div>
-            Donna
-        </div>
+    <header>
+        <div class="content-container">
+            <div class="header-section">
+                <div>
+                    Donna
+                </div>
 
-        <div>
-            Uomo
-        </div>
+                <div>
+                    Uomo
+                </div>
 
-        <div>
-            Bambini
-        </div>
-    </div>
+                <div>
+                    Bambini
+                </div>
+            </div>
     
 
-    <div>
-        logo
-    </div>
+            <div>
+                <img src="../assets/img/boolean-logo.png" alt="">
+            </div>
 
-    <div>
-        <div>
-            emoji
-        </div>
+            <div class="header-section">
+                <div>
+                    emoji
+                </div>
 
-        <div>
-            emoji
-        </div>
+                <div>
+                    emoji
+                </div>
 
-        <div>
-            emoji
+                <div>
+                    emoji
+                </div>
+            </div>
         </div>
-    </div>
+    </header>
+    
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+
+    header{
+        font-size: small;
+
+        color: white;
+
+        background-color: orange;
+        
+        .content-container {
+            width: 80vw;
+            margin: auto;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+        }
+
+        .header-section{
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+        }
+
+        .header-section > div {
+            padding: 20px;
+        }
+
+        img {
+            width: 100px;
+        }
+    }
 
 </style>
