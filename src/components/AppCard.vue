20ef1315be7f4d7dfd9ab051698a87374b008faf
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-02-14 20:03:45 +0100

    add prop for cards

---
+++ src/components/AppCard.vue
@@ -0,0 +1,64 @@
+<script>
+    export default {    
+        props: {
+                img: String,
+                brandName: String,
+                itemName: String,
+                priceValue: String
+                },
+    };
+</script>
+
+<template>
+
+    <div class="card">
+
+        <figure>
+            <img :src="img" alt="">
+        </figure>
+        
+        <div>
+            <div class="card-brand">
+                {{ brandName }}
+            </div>
+
+            <div class="card-name-item">
+                {{ itemName }}
+            </div>
+
+            <div class="card-price">
+                {{ priceValue }}
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<style lang="scss" scoped>
+
+    .card {
+        width: calc(100% / 3 - 40px);
+        margin: 20px;
+        display: flex;
+        flex-direction: column;
+    }
+
+    img{
+        width: 100%;
+        object-fit: contain;
+    }
+
+    .card-brand {
+        font-size: smaller;
+    }
+
+    .card-name-item {
+        font-weight: bold;
+    }
+
+    .card-price {
+        font-weight: bolder;
+        color: red;
+    }
+
+</style>
